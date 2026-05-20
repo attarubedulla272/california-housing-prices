eac6cf776aa4d9dec70f04e82175db0c11ad4cb7
attarubedulla272 <ubedullaattar2@gmail.com>
2026-05-20 23:06:59 +0530

    change in main.py files

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 # load of dataset to predict the housing prices in California
+from ast import main
+import email
+from unicodedata import name
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.model_selection import StratifiedShuffleSplit
 
 
 # load the dataset
 load_data=pd.read_csv("data/housing.csv")
 print(load_data.head())
-# check for null values
+
+
